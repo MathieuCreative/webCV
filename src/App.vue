d485141d14a23d0b898c3797b1692b86cb4c45e1
mathieu <mathieu@MacBook-Air-de-Paul.local>
2024-02-10 20:33:32 +0100

    Tout fonctionne

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue';
+import { defineComponent, ref, onMounted, onUnmounted } from 'vue';
 
 interface Skill {
   name: string;
@@ -11,31 +11,65 @@ export default defineComponent({
   name: 'SkillBar',
   setup() {
     const skills = ref<Skill[]>([
-      { name: 'HTML', level: 90, animatedLevel: 0 },
-      { name: 'CSS', level: 80, animatedLevel: 0 },
-      { name: 'JS', level: 75, animatedLevel: 0 },
-      { name: 'HTML', level: 90, animatedLevel: 0 },
-      { name: 'CSS', level: 80, animatedLevel: 0 },
-      { name: 'JS', level: 75, animatedLevel: 0 },
-      { name: 'HTML', level: 90, animatedLevel: 0 },
-      { name: 'CSS', level: 80, animatedLevel: 0 },
-      { name: 'JS', level: 75, animatedLevel: 0 },
-      // Autres compétences...
+      { name: 'Java', level: 100, animatedLevel: 0 },
+      { name: 'Flutter', level: 100, animatedLevel: 0 },
+      { name: 'C', level: 80, animatedLevel: 0 },
+      { name: 'Vue.js', level: 75, animatedLevel: 0 },
+      { name: 'Spring Boot', level: 75, animatedLevel: 0 },
+      { name: 'y86', level: 70, animatedLevel: 0 },
+      { name: 'Ocaml', level: 65, animatedLevel: 0 },
+      { name: 'Tailwind', level: 40, animatedLevel: 0 },
+      { name: 'JS CSS HTML', level: 80, animatedLevel: 0 },
+      { name: 'Python', level: 35, animatedLevel: 0 },
     ]);
+    const observer = ref<IntersectionObserver | null>(null);
 
     const animateSkill = (skill: Skill) => {
       let start = 0;
       const interval = setInterval(() => {
         if (start >= skill.level) {
           clearInterval(interval);
+        } else {
+          skill.animatedLevel = start;
+          start++;
         }
-        skill.animatedLevel = start;
-        start++;
-      }, 20); // Ajustez cette valeur pour contrôler la vitesse de l'animation
+      }, 20);
+    };
+
+    const resetAnimation = (skill: Skill) => {
+      skill.animatedLevel = 0; // Réinitialiser immédiatement pour les besoins de l'exemple
+    };
+
+    const observeElement = (el: Element) => {
+      if (observer.value) {
+        observer.value.observe(el);
+      }
     };
 
     onMounted(() => {
-      skills.value.forEach(animateSkill);
+      observer.value = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            skills.value.forEach(animateSkill);
+          } else {
+            skills.value.forEach(resetAnimation); // Réinitialiser quand hors du viewport
+          }
+        });
+      }, {
+        rootMargin: '0px',
+        threshold: 0.1
+      });
+
+      const skillsContainer = document.querySelector('.skills-container');
+      if (skillsContainer) {
+        observeElement(skillsContainer);
+      }
+    });
+
+    onUnmounted(() => {
+      if (observer.value) {
+        observer.value.disconnect();
+      }
     });
 
     return { skills };
@@ -46,41 +80,59 @@ export default defineComponent({
 
 
 
-
 <template>
+
   <div class="custom-bg-color flex flex-wrap md:flex-nowrap ">
     <!-- Zone de texte avec padding et marge -->
     <div class="md:flex-1 p-4 bg-white shadow rounded-lg mr-20 ml-20 my-5">
       <h2 class="text-2xl font-bold mb-4">À propos de moi</h2>
-      <p>Bonjour👋
+      <p>
+        Hello👋
         <br>Je suis Mathieu Magnin, un
 étudiant en 3ème année de Licence Informatique à l’Université de Bordeaux.
-Intrigué par le pouvoir créatif que permet d’acquérir la programmation, je me suis rapidement intéressé à plusieurs technologies ci-contre qui m’ont permis de réaliser quelques projets dont je suis déjà fier !
-L’année prochaine, je souhaite rejoindre l’EINSERB MATMECA en alternance. Je cherche donc une entreprise pour réaliser un stage pendant 4 mois (mai-septembre), et peut-être enchaîner sur une alternance ensemble ensuite !
+<br>
+L’année prochaine, je souhaite rejoindre l’EINSERB-MATMECA en <strong>alternance</strong>. 
+<br>
+Je cherche donc une entreprise pour réaliser un <strong>stage de 4 mois</strong> (1er mai - 1er septembre), et peut-être enchaîner sur une alternance ensemble ensuite !
+<br>
+Comme le montre la variété des technologies que j'ai rencontrées : <strong>tout m'intéresse du moment que ça touche à informatique </strong>!
+<br>
+En parallèle de mes études, j'avance également sur mon gros projet d'application mobile autour du sport "trAIn" avec Flutter, Dart et Riverpod.
+<br>
+N'hésitez pas à me contacter pour en savoir plus !
 </p>
     </div>
 
-    <div class="md:flex-1 p-4 bg-white shadow rounded-lg mr-20 ml-20 my-5">
-  <h2 class="text-2xl font-bold mb-4">Mes compétences</h2>
+    <div class="md:flex-1 p-4 bg-white shadow rounded-lg mr-20 ml-20 my-5 skills-container">
+  <h2 class="text-2xl font-bold mb-4">Technologies rencontrées</h2>
+  <p class="text-gray-400 text-sm mb-4">Attention : 100% = Technologie sur laquelle j'ai passé le plus de temps, et non technologie que je maîtrise à 100% !</p>
   <div class="space-y-4">
     <div v-for="skill in skills" :key="skill.name" class="flex items-center">
       <!-- Conteneur pour le nom de la compétence avec une largeur fixe -->
       <div class="w-20 font-bold">{{ skill.name }}</div>
       <!-- Jauge de compétence -->
       <div class="flex-1 bg-gray-200 rounded-full h-4 dark:bg-gray-700">
-        <div :style="{ width: skill.animatedLevel + '%' }" class="bg-blue-500 h-4 rounded-full transition-all ease-out duration-1000"></div>
+        <div :style="{ width: skill.animatedLevel + '%' }" class="custom-bg-color h-4 rounded-full transition-all ease-out duration-1000"></div>
       </div>
     </div>
   </div>
 </div>
-
 </div>
+<div class="scroll-test"></div>
+
+
 </template>
 
 <style scoped>
 .custom-bg-color {
   background-color: #000C79;
 }
+
+/* Styles pour le div de test de défilement */
+.scroll-test {
+  height: 5000px; /* Ou toute autre hauteur suffisante pour forcer le défilement */
+}
+
 </style>
 
 
